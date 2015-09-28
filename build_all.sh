bd18b3fabc60eec9da7a2f290c17ba7981d96c48
Cheng Kuan <ck@codeck.org>
2015-09-28 07:49:21 +0000

    build latest klm instead klm-1.0

--- build_all.sh
+++ build_all.sh
@@ -6,7 +6,8 @@ docker build --rm=true -t builder -f builder/Dockerfile builder/
 
 rm -rf /home/coreop/core-built/*
 
-docker run --rm=true -ti -v /home/coreop/core-built:/core-built -e "BRANCH=klm-1.0" builder
+
+docker run --rm=true -ti -v /home/coreop/core-built:/core-built builder #default is klm branch
 
 docker run --rm=true -ti  -v /home/coreop/core-built:/core-built -e "BRANCH=master" builder
 
