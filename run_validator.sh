8577ceef1b67477b743e5d751f160ff18a0a1a4d
Cheng Kuan <ck@codeck.org>
2015-09-29 06:14:56 +0000

    expose ports

--- run_validator.sh
+++ run_validator.sh
@@ -2,6 +2,7 @@
 
 docker build --rm=true -t validator -f runner/Dockerfile runner/ \
 	&& docker run --rm=true -ti \
+			  -p=13625:13625 \
 			  -v ~/.aws:/root/.aws \
 			  -v ~/.osscredentials:/root/.osscredentials \
 			  -v ~/core-built.tar.xz:/core-built.tar.xz \
