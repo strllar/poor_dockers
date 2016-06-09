42208c819252be963f799628c30da906ef8dca7b
Cheng Kuan <ck@codeck.org>
2016-06-09 01:43:26 +0000

    expose http port along with "PUBLIC_HTTP_PORT=true" of core configuration

--- run_validator.sh
+++ run_validator.sh
@@ -7,6 +7,7 @@ then
 	docker build --rm=true -t validator -f runner/Dockerfile runner/ \
 		&& docker run --rm=true -ti \
 				  -p=13625:13625 \
+				  -p=127.0.0.1:13625:13625 \
 				  -v ~/.aws:/root/.aws \
 				  -v ~/.osscredentials:/root/.osscredentials \
 				  -v $XZ_FILE:/core-built.tar.xz \
